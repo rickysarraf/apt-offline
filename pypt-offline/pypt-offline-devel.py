0323acc47f5824fa2edadc5a5904b254f241b1b8
riteshsarraf <>
2005-11-15 01:05:42 +0530

    [svn] Many new features are being added. pypt-offline will now allow uris extraction, uris manipulation and data manipulation; all from its own _single_ interface

--- pypt-offline/pypt-offline-devel.py
+++ pypt-offline/pypt-offline-devel.py
@@ -18,7 +18,7 @@
 #                                                                          #
 #    You should have received a copy of the GNU General Public License     #
 #    along with this program; if not, write to the                         #
-#    Free Software Foundation, Inc.,                                       #
+#    Free Software F[-d] [-s] [-u]oundation, Inc.,                         #
 #    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ############################################################################
 
@@ -35,7 +35,7 @@ def download_from_web(sUrl, sFile, sSourceDir):
         #decode_exceptions(X)
         print errno, strerror
         errfunc(errno)
-        #errfunc(decode_exceptions(X))
+        #errfunc(decod[-d] [-s] [-u]e_exceptions(X))
     except OSError, (errno, strerror):
         print errno, strerror
         errfunc(errno)
@@ -79,16 +79,17 @@ def walk_tree_copy_debs(sRepository, sFile, sSourceDir):
           Else it fetches the package from the net"""
     bFound = False
     try:
-        for name in os.listdir(sRepository):
-            path = os.path.join(sRepository, name)
-            if os.path.isdir(path):
-                walk_tree_copy_debs(path, sFile, sSourceDir)
-            elif name.endswith('.deb') or name.endswith('.rpm'):
-                if name == sFile:
-                    shutil.copy(path, sSourceDir)
-                    bFound = True
-                    break    
-        return bFound
+        if sRepository is not None:
+            for name in os.listdir(sRepository):
+                path = os.path.join(sRepository, name)
+                if os.path.isdir(path):
+                    walk_tree_copy_debs(path, sFile, sSourceDir)
+                elif name.endswith('.deb') or name.endswith('.rpm'):
+                    if name == sFile:
+                        shutil.copy(path, sSourceDir)
+                        bFound = True
+                        break    
+            return bFound
     except OSError, (errno, strerror):
         print errno, strerror
         errfunc(errno)
@@ -147,9 +148,9 @@ def report(blockcount, bytesdownloaded, totalbytes):
 if __name__ == "__main__":
     
     try:
-        version = "0.5b"
+        version = "0.6b"
         reldate = "03/10/2005"
-        copyright = "(C) 2005 Ritesh Raj Sarraf <rrs@researchut.com>"
+        copyright = "(C) 2005 Ritesh Raj Sarraf - RESEARCHUT (http://www.researchut.com/)"
     
         #FIXME: Option Parsing
         # There's a flaw with either optparse or I'm not well understood with it
@@ -158,25 +159,69 @@ if __name__ == "__main__":
         
         #parser = OptionParser()
         #parser = optparse.OptionParser()
-        parser = optparse.OptionParser(usage="%prog [-d] [-s] [-u]", version="%prog " + version)
-        parser.add_option("-d","--download", dest="download_path", help="Root directory path to save files", action="store", type="string")
-        parser.add_option("-s","--search", dest="search_path", help="Root directory path where the pre-downloaded files will be searched. If not, give a period '.'",action="store", type="string")
-        parser.add_option("-u","--uris", dest="uris_file", help="Full path of the uris file",action="store", type="string")
+        parser = optparse.OptionParser(usage="%prog [OPTION1, OPTION2, ...]", version="%prog " + version)
+        parser.add_option("-d","--download-dir", dest="download_dir", help="Root directory path to save the downloaded files", action="store", type="string")
+        parser.add_option("-s","--cache-dir", dest="cache_dir", help="Root directory path where the pre-downloaded files will be searched. If not, give a period '.'",action="store", type="string", metavar=".")
+        parser.add_option("-u","--uris", dest="uris_file", help="Full path of the uris file which contains the main database of files to be downloaded",action="store", type="string")
+        
+        #TODO: Add updation
+        # The new plan is to make pypt-offline do the updation and upgradation from within its own interface instead of expecting
+        # the user to do the dirty part. We'll add options which'll take care of it.
+        # For this we'll have additional options
+        # --set-update - This will extract the list of uris which need to be fetched for _updation_. This command must be executed on the NONET machine.
+        # --fetch-update - This will fetch the list of uris which need for apt's databases _updation_. This command must be executed on the WITHNET machine.
+        # --install-update - This will install the fetched database files to the  NONET machine and _update_ the apt database on the NONET machine. This command must be executed on the NONET machine.
+        # The same will happen for upgradation.
+        # --set-upgrade - This will extract the list of uris which need to be fetched for _upgradation_. This command must be executed on the NONET machine.
+        # --fetch-upgrade - This will fetch the list of uris which need for apt's databases _upgradation_. This command must be executed on the WITHNET machine.
+        # --install-upgrade - This will install the fetched database files to the  NONET machine and _upgrade_ the packages. This command must be executed on the NONET machine.
+        parser.add_option("","--set-update", dest="set_update", help="Extract the list of uris which need to be fetched for _updation_", action="store_true", default=False)
+        parser.add_option("","--fetch-update", dest="fetch_update", help="Fetch the list of uris which are needed for apt's databases _updation_. This command must be executed on the WITHNET machine", action="store_true", default=False, metavar="pypt-offline-update.dat")
+        parser.add_option("","--install-update", dest="install_update", help="Install the fetched database files to the  NONET machine and _update_ the apt database on the NONET machine. This command must be executed on the NONET machine", action="store_true", default=False, metavar="pypt-offline-update-fetched.zip")
+        parser.add_option("","--set-upgrade", dest="set_upgrade", help="Extract the list of uris which need to be fetched for _upgradation_", action="store_true", default=False)
+        parser.add_option("","--fetch-upgrade", dest="fetch_upgrade", help="Fetch the list of uris which are needed for apt's databases _upgradation_. This command must be executed on the WITHNET machine", action="store_true", default=False, metavar="pypt-offline-upgrade.dat")
+        parser.add_option("","--install-upgrade", dest="install_upgrade", help="Install the fetched packages to the  NONET machine and _upgrade_ the packages on the NONET machine. This command must be executed on the NONET machine", action="store_true", default=False, metavar="/var/cache/apt/archives/pypt-offline-upgrade-fetched.dat")
         (options, arguments) = parser.parse_args()
         #parser.check_required("-d", "-s", "-u")
         #if len(arguments) != 2:
         #    parser.error("Err! Incorrect number of arguments. Exiting")
         
-        sSourceDir = options.download_path
-        sRawUris = options.uris_file
-        sRepository = options.search_path
+        sSourceDir = options.download_dir
+        if sSourceDir is None:
+            if os.access("pypt-offline-downloaded-files", os.W_OK) is True:
+                sSourceDir = "pypt-offline-downloaded-files"
+            else:
+                os.mkdir("pypt-offline-downloaded-files")
+                sSourceDir = "pypt-offline-downloaded-files"
         
+        sRawUris = options.uris_file
+        if sRawUris is None:
+            if os.access("pypt-offline-update.dat", os.R_OK) is True:
+                sRawUris = "pypt-offline-update.dat"
+        
+        sRepository = options.cache_dir
+        #if sRepository is None:
+        #    sRepository = os.curdir
+            
         print "pypt-offline %s" % version
         print "Copyright %s" % copyright
         print """\n\nThis program is still in it's very early stage. There can be situations
         where things might not work as expected. Please direct all errors, bugs,suggestions
         etc to me at rrs@researchut.com\n\n\n"""
+        
+        if options.set_update is True:
+            #FIXME: More platforms need to be added
+            if sys.platform != "linux2":
+                print "This argument is supported only on Unix like systems with apt installed\n"
+            else:
+                #TODO: Implement --set-update using _maybe_ apt
+                print "To be implemented\n"
 
+        if options.fetch_update is True:
+            #TODO: Updation
+            # Implement below similar code for updation
+            print "Fetching uris which update apt's package database\n"
+            
         # Let's first open the RAW_URIS file and read it all.
         try:
             lRawData = open(sRawUris, 'r').readlines()
