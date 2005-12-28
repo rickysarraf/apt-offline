fe85a3e4236fdc40f2d0bf30c8cfa29ddec72f22
Ritesh Raj Sarraf <rrs@researchut.com>
2005-12-29 02:34:41 +0530

    [svn] Some major feature rework. Don't use, it's broken right now

--- pypt-offline/pypt-offline-devel.py
+++ pypt-offline/pypt-offline-devel.py
@@ -71,12 +71,14 @@ def download_from_web(sUrl, sFile, sSourceDir):
 # But for now it's doing the job.
 # Need to find a better algorithm, maybe os.walk()                    
 def walk_tree_copy_debs(sRepository, sFile, sSourceDir):
-    """The core algorithm is here for the whole program to function'\n'
-          It recursively searches a tree/subtree of folders for package files'\n'
-          like the directory structure of "apt-proxy". If files are found (.deb || .rpm)'\n'
-          it checks wether they are on the list of packages to be fetched. If yes,'\n\
-          it copies them. Same goes for flat "apt archives folders" also.'\n'
-          Else it fetches the package from the net"""
+    """This function checks for a package to see if its already downloaded
+    It can search directories with depths."""
+    #The core algorithm is here for the whole program to function'\n'
+    #It recursively searches a tree/subtree of folders for package files'\n'
+    #like the directory structure of "apt-proxy". If files are found (.deb || .rpm)'\n'
+    #it checks wether they are on the list of packages to be fetched. If yes,'\n\
+    #it copies them. Same goes for flat "apt archives folders" also.'\n'
+    #Else it fetches the package from the net"""
     bFound = False
     try:
         if sRepository is not None:
@@ -144,7 +146,47 @@ def report(blockcount, bytesdownloaded, totalbytes):
 #        # Assumes the options's default is set to 'None'
 #        if getattr(self.values, option.dest) is None:
 #            self.error("%s option not supplied" % x)
+# Let's first open the RAW_URIS file and read it all.
+def starter(uri, path, cache, type):
+    
+    #sSourceDir = path
+    if path is None:
+        if os.access("pypt-downloads", os.W_OK) is True:
+            sSourceDir = "pypt-downloads"
+        else:
+            os.mkdir("pypt-downloads")
+            sSourceDir = "pypt-downloads"
+    else:
+        sSourceDir = path
             
+    if cache is None:
+        sRepository = os.curdir
+    else:
+        sRepository = cache
+        
+    try:
+        lRawData = open(uri, 'r').readlines()
+    except IOError, (errno, strerror):
+        print errno, strerror
+        errfunc(errno)
+    
+    for each_single_item in lRawData:
+        lSplitData = each_single_item.split(' ') # Split on the basis of ' ' i.e. space
+
+        # We initialize the variables "sUrl" and "sFile" here.
+        # We also strip the single quote character "'" to get the real data
+        sUrl = string.rstrip(string.lstrip(''.join(lSplitData[0]), chars="'"), chars="'")
+        sFile = string.rstrip(string.lstrip(''.join(lSplitData[1]), chars="'"), chars="'")
+        bStatus = walk_tree_copy_debs(sRepository, sFile, sSourceDir)
+        if bStatus == True:
+            print sFile + " sccessfully copied from local cache.\n"
+        else:
+            bStatus = download_from_web(sUrl, sFile, sSourceDir)
+            if bStatus == True:
+                 print sFile + " successfully downloaded from web\n."
+            else:
+                 print sFile + " not downloaded from web and NA in local cache.\n"
+           
 if __name__ == "__main__":
     
     try:
@@ -160,8 +202,11 @@ if __name__ == "__main__":
         #parser = OptionParser()
         #parser = optparse.OptionParser()
         parser = optparse.OptionParser(usage="%prog [OPTION1, OPTION2, ...]", version="%prog " + version)
-        parser.add_option("-d","--download-dir", dest="download_dir", help="Root directory path to save the downloaded files", action="store", type="string")
+        parser.add_option("-d","--download-dir", dest="download_dir", help="Root directory path to save the downloaded files", action="store", type="string", metavar="pypt-downloads")
+        #parser.set_defaults(download_dir="pypt-downloads")
         parser.add_option("-s","--cache-dir", dest="cache_dir", help="Root directory path where the pre-downloaded files will be searched. If not, give a period '.'",action="store", type="string", metavar=".")
+        #parser.set_defaults(cache_dir=".")
+        #parser.set_defaults(cache_dir=".")
         parser.add_option("-u","--uris", dest="uris_file", help="Full path of the uris file which contains the main database of files to be downloaded",action="store", type="string")
         
         #TODO: Add updation
@@ -175,76 +220,102 @@ if __name__ == "__main__":
         # --set-upgrade - This will extract the list of uris which need to be fetched for _upgradation_. This command must be executed on the NONET machine.
         # --fetch-upgrade - This will fetch the list of uris which need for apt's databases _upgradation_. This command must be executed on the WITHNET machine.
         # --install-upgrade - This will install the fetched database files to the  NONET machine and _upgrade_ the packages. This command must be executed on the NONET machine.
-        parser.add_option("","--set-update", dest="set_update", help="Extract the list of uris which need to be fetched for _updation_", action="store_true", default=False)
-        parser.add_option("","--fetch-update", dest="fetch_update", help="Fetch the list of uris which are needed for apt's databases _updation_. This command must be executed on the WITHNET machine", action="store_true", default=False, metavar="pypt-offline-update.dat")
-        parser.add_option("","--install-update", dest="install_update", help="Install the fetched database files to the  NONET machine and _update_ the apt database on the NONET machine. This command must be executed on the NONET machine", action="store_true", default=False, metavar="pypt-offline-update-fetched.zip")
-        parser.add_option("","--set-upgrade", dest="set_upgrade", help="Extract the list of uris which need to be fetched for _upgradation_", action="store_true", default=False)
-        parser.add_option("","--fetch-upgrade", dest="fetch_upgrade", help="Fetch the list of uris which are needed for apt's databases _upgradation_. This command must be executed on the WITHNET machine", action="store_true", default=False, metavar="pypt-offline-upgrade.dat")
-        parser.add_option("","--install-upgrade", dest="install_upgrade", help="Install the fetched packages to the  NONET machine and _upgrade_ the packages on the NONET machine. This command must be executed on the NONET machine", action="store_true", default=False, metavar="/var/cache/apt/archives/pypt-offline-upgrade-fetched.dat")
-        (options, arguments) = parser.parse_args()
+        parser.add_option("","--set-update", dest="set_update", help="Extract the list of uris which need to be fetched for _updation_", action="store", type="string", metavar="pypt-offline-update.dat")
+        #parser.set_defaults(set_update="pypt-offline-update.dat")
+        parser.add_option("","--fetch-update", dest="fetch_update", help="Fetch the list of uris which are needed for apt's databases _updation_. This command must be executed on the WITHNET machine", action="store", type="string", metavar="pypt-offline-update.dat")
+        #parser.set_defaults(fetch_update="pypt-offline-update.dat")
+        parser.add_option("","--install-update", dest="install_update", help="Install the fetched database files to the  NONET machine and _update_ the apt database on the NONET machine. This command must be executed on the NONET machine", action="store", type="string", metavar="pypt-offline-update-fetched.zip")
+        #parser.set_defaults(install_update="pypt-offline-update-fetched.zip")
+        parser.add_option("","--set-upgrade", dest="set_upgrade", help="Extract the list of uris which need to be fetched for _upgradation_", action="store", type="string", metavar="pypt-offline-upgrade.dat")
+        #parser.set_defaults(set_upgrade="pypt-offline-upgrade.dat")
+        parser.add_option("","--upgrade-type", dest="upgrade_type", help="Type of upgrade to do. Use one of upgrade, dist-upgrade, dselect-ugprade", action="store", type="string", metavar="upgrade")
+        #parser.set_defaults(upgrade_type="upgrade")
+        parser.add_option("","--fetch-upgrade", dest="fetch_upgrade", help="Fetch the list of uris which are needed for apt's databases _upgradation_. This command must be executed on the WITHNET machine", action="store", type="string", metavar="pypt-offline-upgrade.dat")
+        #parser.set_defaults(fetch_upgrade="pypt-offline-upgrade.dat")
+        parser.add_option("","--install-upgrade", dest="install_upgrade", help="Install the fetched packages to the  NONET machine and _upgrade_ the packages on the NONET machine. This command must be executed on the NONET machine", action="store", type="string", metavar="pypt-offline-update-fetched.zip")
+        #parser.set_defaults(install_ugprade="pypt-offline-update-fetched.zip")
+        (options, args) = parser.parse_args()
         #parser.check_required("-d", "-s", "-u")
         #if len(arguments) != 2:
         #    parser.error("Err! Incorrect number of arguments. Exiting")
+        #print len(args)
+        #if len(options) != 1 or len(options) > 2:
+        #    print len(args)
+        #    parser.error("No arguments were passed\n")
+        #    sys.exit(1)
+            
         
-        sSourceDir = options.download_dir
-        if sSourceDir is None:
-            if os.access("pypt-offline-downloaded-files", os.W_OK) is True:
-                sSourceDir = "pypt-offline-downloaded-files"
-            else:
-                os.mkdir("pypt-offline-downloaded-files")
-                sSourceDir = "pypt-offline-downloaded-files"
+        #sRawUris = options.uris_file
+        #if sRawUris is None:
+        #    if os.access("pypt-offline-update.dat", os.R_OK) is True:
+        #        sRawUris = "pypt-offline-update.dat"
         
-        sRawUris = options.uris_file
-        if sRawUris is None:
-            if os.access("pypt-offline-update.dat", os.R_OK) is True:
-                sRawUris = "pypt-offline-update.dat"
-        
-        sRepository = options.cache_dir
-        #if sRepository is None:
-        #    sRepository = os.curdir
             
         print "pypt-offline %s" % version
         print "Copyright %s" % copyright
-        print """\n\nThis program is still in it's very early stage. There can be situations
-        where things might not work as expected. Please direct all errors, bugs,suggestions
-        etc to me at rrs@researchut.com\n\n\n"""
+        #print """\n\nThis program is still in it's very early stage. There can be situations
+        #where things might not work as expected. Please direct all errors, bugs,suggestions
+        #etc to me at rrs@researchut.com\n\n\n"""
         
-        if options.set_update is True:
-            #FIXME: More platforms need to be added
-            if sys.platform != "linux2":
-                print "This argument is supported only on Unix like systems with apt installed\n"
+        print type(options.set_update)
+        if options.set_update:
+            if os.getuid() != 0:
+                parser.error("This option requires super-user privileges. Execute as root or use sudo/su")
+            #options.set_update = "pypt-offline-update.dat"
+            #FIXME: More platforms need to be added -- FIXED
+            # We're using linux2, gnu0 and gnukfreebsd5 as the only platforms because these are the only platforms
+            # upon which apt has been ported under Debian.
+            if sys.platform == "linux2" or sys.platform == "gnu0" or sys.platform == "gnukfreebsd5":
+                print "Generating database of files that are needed for an update.\n"
+                os.chdir(options.set_update)
+                os.system('/usr/bin/apt-get -qq --print-uris update > pypt-offline-update.dat')
             else:
+                parser.error("This argument is supported only on Unix like systems with apt installed\n")
                 #TODO: Implement --set-update using _maybe_ apt
-                print "To be implemented\n"
+            sys.exit(0)
 
-        if options.fetch_update is True:
+        if options.set_upgrade or options.upgrade_type:
+            if not (options.set_upgrade and options.upgrade_type):
+                parser.error("Options --set-upgrade and --upgrade-type are mutually inclusive\n")
+            else:
+                if os.getuid() != 0:
+                    parser.error("This option requires super-user privileges. Execute as root or use sudo/su")
+                
+                if sys.platform == "linux2" or sys.platform == "gnu0" or sys.platform == "gnukfreebsd5":
+                    os.chdir(options.set_upgrade)
+                    if options.upgrade_type == "upgrade":
+                        print "Generating database of files that are needed for an upgrade.\n"
+                        os.system('/usr/bin/apt-get -qq --print-uris upgrade > pypt-offline-upgrade.data')
+                    elif options.upgrade_type == "dist-upgrade":
+                        print "Generating database of files that are needed for an upgrade.\n"
+                        os.system('/usr/bin/apt-get -qq --print-uris dist-upgrade > pypt-offline-upgrade.data')
+                    elif options.upgrade_type == "dselect-upgrade":
+                        print "Generating database of files that are needed for an upgrade.\n"
+                        os.system('/usr/bin/apt-get -qq --print-uris dselect-upgrade > pypt-offline-upgrade.data')
+                    else:
+                        parser.error("Invalid upgrade argument type selected\nPlease use one of, upgrade/dist-upgrade/dselect-upgrade\n")
+                else:
+                    parser.error("This argument is supported only on Unix like systems with apt installed\n")
+                sys.exit(0)
+            
+        if options.fetch_update:
             #TODO: Updation
             # Implement below similar code for updation
             print "Fetching uris which update apt's package database\n"
             
-        # Let's first open the RAW_URIS file and read it all.
-        try:
-            lRawData = open(sRawUris, 'r').readlines()
-        except IOError, (errno, strerror):
-            print errno, strerror
-            errfunc(errno)
-    
-        for each_single_item in lRawData:
-            lSplitData = each_single_item.split(' ') # Split on the basis of ' ' i.e. space
-
-            # We initialize the variables "sUrl" and "sFile" here.
-            # We also strip the single quote character "'" to get the real data
-            sUrl = string.rstrip(string.lstrip(''.join(lSplitData[0]), chars="'"), chars="'")
-            sFile = string.rstrip(string.lstrip(''.join(lSplitData[1]), chars="'"), chars="'")
-            bStatus = walk_tree_copy_debs(sRepository, sFile, sSourceDir)
-            if bStatus == True:
-                print sFile + " sccessfully copied from local cache.\n"
-            else:        
-                bStatus = download_from_web(sUrl, sFile, sSourceDir)
-                if bStatus == True:
-                    print sFile + " successfully downloaded from web\n."
-                else:
-                    print sFile + " not downloaded from web and NA in local cache.\n"
+            # This function will take two arguments. uris file and download type
+            # If the uris file is an update database it shouldn't do filetype (.deb/.rpm)
+            # checking.
+            # Since we're in fetch_update, the download_type will be non-deb/rpm data
+            #foo(sRawUris, download_type)
+            # 1 is for update packages and 2 is for upgrade packages
+            download_type = 1
+            starter(options.fetch_update, options.download_dir, options.cache_dir, download_type)
+            
+            #if sRawUris is None:
+            #    if os.access("pypt-offline-update.dat", os.R_OK) is True:
+            #        sRawUris = "pypt-offline-update.dat"
+            
     
     except KeyboardInterrupt:
         print "\nReceived immediate EXIT signal. Exiting!\n"
