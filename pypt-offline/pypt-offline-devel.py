b8c054926691faa334af39ec3bf539e10de0bfb4
riteshsarraf <>
2006-03-09 03:05:42 +0530

    [svn]

--- pypt-offline/pypt-offline-devel.py
+++ pypt-offline/pypt-offline-devel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # pypt-offline.py
 # version devel
+# Release: svn_test
 
 ############################################################################
 #    Copyright (C) Ritesh Raj Sarraf                                       #
