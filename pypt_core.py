01dc175763f02b7f1ffa88d36c567f52716308ea
Ritesh Raj Sarraf <rrs@researchut.com>
2007-04-25 20:25:41 +0530

    [svn] * This should be better. Most files which aren't required for bug details fetch are not required for my purpose. Also there are minor import modifications in debianbts.py because I don't need the full reportbug suite. * This hopefully should be the final package selection

--- pypt_core.py
+++ pypt_core.py
@@ -282,6 +282,32 @@ class Archiver:
             return False
 
 
+def FetchBugReportsDebian(PackageName, FileHandle):
+    try:
+        import debianbts
+    except ImportError:
+        return False
+    
+    bug_list = []
+    file_handle = open("FileHandle", 'w')
+    
+    (num_of_bugs, header, bugs_list) = debianbts.get_reports(package)
+
+    if num_of_bugs:
+        for x in bugs_list:
+            (sub_bugs_header, sub_bugs_list) = x
+            for x in sub_bugs_list:
+                break_bugs = x.split(':')
+                bug_num = string.lstrip(break_bugs[0], '#')
+                data = debianbts.get_report(bug_num, followups=True)
+                file_handle.write(data[0] + "\n\n")
+                for x in data[1]:
+                    file_handle.write(x)
+                    file_handle.write("\n")
+                file_handle.write("\n\n\n")
+                file_handle.flush()
+    
+    
 def files(root): 
     for path, folders, files in os.walk(root): 
         for file in files: 
