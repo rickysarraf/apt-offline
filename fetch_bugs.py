f60b878690de43d28361b46979fc2177960ba12c
riteshsarraf <>
2007-05-24 17:56:56 +0530

    [svn] * Removed --warnings. One is sufficient * Changed the BugReport code to a class * Better logic in the if/else constructs in fetcher() of when to fetch bugreports with zip

--- fetch_bugs.py
+++ fetch_bugs.py
@@ -1,8 +1,17 @@
-from pypt_core import FetchBugReportsDebian
+#!/usr/bin/env python
+
+import pypt_core
 
 package = raw_input("Please enter the Debian package name: ")
 file = raw_input("Please enter the filename with full path: ")
 
 
-if FetchBugReportsDebian(package, file) is True:
+#if pypt_core.FetchBugReportsDebian(package, file) is True:
+#    print "Wrote bug report details for package %s to file %s.\n" % (package, file)
+
+BugReportDebian = pypt_core.FetchBugReports()
+
+if BugReportDebian.FetchBugsDebian(package, file) is True:
     print "Wrote bug report details for package %s to file %s.\n" % (package, file)
+else:
+    print "No bugs found for package %s.\n" % (package)
