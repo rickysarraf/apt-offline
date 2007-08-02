d89e991e770854b3ad0e5419db3347b197094b29
Ritesh Raj Sarraf <rrs@researchut.com>
2007-08-03 01:48:20 +0530

    [fetch_bugy.py] * Strip the carriage returns * Move bugTypes to global * Align FetchBugReports() call
    [pypt_core.py]
    * Bring bugTypes to global
    * Some stupid fixes in the offline bug support in syncer()
    * syncer() offline bug report is broken currently.
    * syncer() wasn't used for long and was completely broken. options wasn't
    * supposed to be passed to it. Only install_upgrade and apt_$$_$$_type was
    * required.

--- fetch_bugs.py
+++ fetch_bugs.py
@@ -3,13 +3,17 @@
 import pypt_core
 
 package = raw_input("Please enter the Debian package name: ")
+package = package.rstrip("\r")
 file = raw_input("Please enter the filename with full path: ")
+file = file.rstrip("\r")
+
+bugTypes = ["Resolved bugs", "Normal bugs", "Minor bugs", "Wishlist items", "FIXED"]
 
 
 #if pypt_core.FetchBugReportsDebian(package, file) is True:
 #    print "Wrote bug report details for package %s to file %s.\n" % (package, file)
 
-BugReportDebian = pypt_core.FetchBugReports()
+BugReportDebian = pypt_core.FetchBugReports(file, bugTypes)
 
 if BugReportDebian.FetchBugsDebian(package, file) is True:
     print "Wrote bug report details for package %s to file %s.\n" % (package, file)
