087fba2531fa49c69f548e8a16aea1c14f9d3d6b
Ritesh Raj Sarraf <rrs@researchut.com>
2007-08-11 03:41:19 +0530

    * Fuck!! Why always such stupidities?

--- pypt_core.py
+++ pypt_core.py
@@ -258,7 +258,7 @@ class Log:
         
         if self.platform == 'posix':
             sys.stdout.write(self.color_syntax + self.color[color])
-            sys.stderr.write(self.color_syntax + self.color(color))
+            sys.stderr.write(self.color_syntax + self.color[color])
         elif self.platform == 'microsoft':
             WConio.textcolor(self.color[color])
             
