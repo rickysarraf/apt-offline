6155c48077fd3f2096ae292826f494530607810c
Ritesh Raj Sarraf <rrs@researchut.com>
2008-04-14 20:00:35 +0530

    Testcase for pypt-offline on Windows

---
+++ pypt-offline-testcase.bat
@@ -0,0 +1,25 @@
+@echo pypt-offline Test Cases
+
+python.exe pypt-offline --fetch-update C:\update.uris --threads 5 --zip -d C:\test
+rmdir /q /s C:\test
+ping -n 10 localhost 1>null
+
+python.exe pypt-offline --fetch-update C:\update.uris --threads 5 --zip
+rmdir /q /s pypt-downloads
+ping -n 10 localhost 1>null
+
+python.exe pypt-offline --fetch-update C:\update.uris --threads 5
+rmdir /q /s pypt-downloads
+ping -n 10 localhost 1>null
+
+python.exe pypt-offline --fetch-update C:\update.uris --zip
+rmdir /q /s pypt-downloads
+ping -n 10 localhost 1>null
+
+python.exe pypt-offline --fetch-update C:\update.uris --threads 5 -d C:\test
+rmdir /q /s C:\test
+ping -n 10 localhost 1>null
+
+python.exe pypt-offline --fetch-upgrade C:\upgrade.uris --threads 5 --zip -d C:\test
+rmdir /q /s C:\test
+ping -n 10 localhost 1>null
