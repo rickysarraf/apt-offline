c5bc5f5514c0f9c47f8235da0fbb81f8e5cd79f5
Ritesh Raj Sarraf <rrs@researchut.com>
2008-04-14 21:14:24 +0530

    If a download_dir is provided, check if it exists and is writeable If doesn't exist, create it.

--- pypt-offline-testcase.bat
+++ pypt-offline-testcase.bat
@@ -23,3 +23,7 @@ ping -n 10 localhost 1>null
 python.exe pypt-offline --fetch-upgrade C:\upgrade.uris --threads 5 --zip -d C:\test
 rmdir /q /s C:\test
 ping -n 10 localhost 1>null
+
+python.exe pypt-offline --fetch-upgrade C:\upgrade.uris --threads 5 --zip -d C:\test --fetch-bug-reports
+rmdir /q /s C:\test
+ping -n 10 localhost 1>null
