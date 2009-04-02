ac0b4a876771e2da5876ed97f65bf1509f82e5dc
Ritesh Raj Sarraf <rrs@researchut.com>
2009-04-03 03:07:11 +0530

    Update copyright date

--- pypt_core.py
+++ pypt_core.py
@@ -1,6 +1,6 @@
 '''    
     pypt-offline  -- An offline package manager for Debian and its derivatives
-    Copyright (C) 2007  Ritesh Raj Sarraf
+    Copyright (C) 2007 - 2009  Ritesh Raj Sarraf
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
