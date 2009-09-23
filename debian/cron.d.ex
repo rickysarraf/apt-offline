ad5f4dd1e6c84c96b178672725438dd659b29f2c
Ritesh Raj Sarraf <rrs@researchut.com>
2009-09-23 14:26:10 +0530

    Filter out the unwanted debian/ files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the apt-offline package
-#
-0 4	* * *	root	[ -x /usr/bin/apt-offline_maintenance ] && /usr/bin/apt-offline_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(apt-offline):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="apt-offline" command="/usr/bin/apt-offline"
