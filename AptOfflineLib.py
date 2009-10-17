f38ea0a251cd9e93e51a7e5ccf071ac93515b272
Ritesh Raj Sarraf <rrs@researchut.com>
2009-10-18 00:24:29 +0530

    add space to format current/total items properly

--- AptOfflineLib.py
+++ AptOfflineLib.py
@@ -317,7 +317,7 @@ class ProgressBar( object ):
                 self.display()
         
         def display( self ):
-                print "\r%3s/%3s items: %s\r" % ( self.complete, self.items, str( self ) ),
+                print "\r%3s /%3s items: %s\r" % ( self.complete, self.items, str( self ) ),
         
         def __str__( self ):
                 #compute display fraction
