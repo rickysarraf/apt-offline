e484adecafccd511213f657510f0a1ee5d2e8b6e
Manish Sinha <manishsinha.tech@gmail.com>
2010-02-05 02:56:38 +0530

    Created About Dialog and attached it to Main Dialog
    Changes:
    * Created AptOfflineQtAbout.ui file
    * Created Ui_AptOfflineQtAbout.py file for holding the UI
    * Created AptOfflineQtAbout.py for calling the UI
    * Added the Icons for Application Exit
    * Edited the Icon resource file to accomodate for application exit icon

---
+++ ui/AptOfflineQtAbout.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt4 import QtCore, QtGui
+
+from Ui_AptOfflineQtAbout import Ui_AboutAptOffline
+
+
+class AptOfflineQtAbout(QtGui.QDialog):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_AboutAptOffline()
+        self.ui.setupUi(self)
+        
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = AptOfflineQtAbout()
+    myapp.show()
+    sys.exit(app.exec_())
