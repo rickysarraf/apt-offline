56014343fef7addd043400237367444d559f9b8c
Abhishek Mishra <ideamonk@gmail.com>
2010-02-13 17:58:04 +0530

    added GetterArgs to UiDataStructs

--- apt_offline_gui/UiDataStructs.py
+++ apt_offline_gui/UiDataStructs.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class SetterArgs():
     
     def __init__(self, filename, update, upgrade, install_packages):
@@ -6,7 +7,7 @@ class SetterArgs():
         # self.set_update is of type boolean
         self.set_update = update
         
-        # self.set_upgrad can be either True or False
+        # self.set_upgrade can be either True or False
         self.set_upgrade = upgrade
         self.upgrade_type = "upgrade"
         
@@ -27,3 +28,35 @@ class SetterArgs():
         
         return ""
     
+class GetterArgs():
+
+    def __init__(self, filename=None, bundle_file=None, socket_timeout=30, \
+                    num_of_threads=1, disable_md5_check=True, deb_bugs=False,
+                        download_dir=None, cache_dir=None):
+
+        self.get = filename
+
+        # TODO: to be implemented in next revision
+        self.socket_timeout = socket_timeout
+        self.num_of_threads = num_of_threads
+
+        self.bundle_file = bundle_file
+        self.disable_md5_check = disable_md5_check
+        self.deb_bugs = deb_bugs
+        self.download_dir = download_dir
+        self.cache_dir = cache_dir
+
+
+    def __str__(self):
+        print "self.get=",self.get
+        print "self.filename=",self.filename
+        print "self.bundle_file=",self.bundle_file
+        print "self.socket_timeout=",self.socket_timeout
+        print "self.num_of_threads=",self.num_of_threads
+        print "self.disable_md5_check=",self.disable_md5_check
+        print "self.deb_bugs=",self.deb_bugs
+        print "self.download_dir=",self.download_dir
+        print "self.cache_dir=",self.cache_dir
+        
+        return ""
+
