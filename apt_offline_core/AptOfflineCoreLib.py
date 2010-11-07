cd7e92e0a85c30372ce9916a7fa5692c4a004051
Ritesh Raj Sarraf <rrs@researchut.com>
2010-11-07 22:00:28 +0530

    bump version to 1.0

--- apt_offline_core/AptOfflineCoreLib.py
+++ apt_offline_core/AptOfflineCoreLib.py
@@ -80,7 +80,7 @@ figuring out if the packages are in the local cache, handling exceptions and man
 
 
 app_name = "apt-offline"
-version = "0.9.9"
+version = "1.0"
 copyright = "(C) 2005 - 2010 Ritesh Raj Sarraf"
 terminal_license = "This program comes with ABSOLUTELY NO WARRANTY.\n\
 This is free software, and you are welcome to redistribute it under\n\
