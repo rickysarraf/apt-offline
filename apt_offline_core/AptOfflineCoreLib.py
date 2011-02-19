b35ef11d1d4b282f5fe2d69b4a2b0d4876ec96c1
root <root@champaran.hq.netapp.com>
2011-02-19 21:09:13 +0530

    Fix old junk code
    This old junk code imported gui libs into AptOfflineCoreLib thus making apt-offline pkg
    dependent on apt-offline-gui pkg. Thanks to Abhishek Mishra and myself for the root cause :-)

--- apt_offline_core/AptOfflineCoreLib.py
+++ apt_offline_core/AptOfflineCoreLib.py
@@ -67,7 +67,6 @@ except ImportError:
 PythonApt = False #Remove it after porting to python-apt
     
 import AptOfflineLib
-import apt_offline_gui.QtProgressBar
 
 #INFO: Set the default timeout to 30 seconds for the packages that are being downloaded.
 socket.setdefaulttimeout(30)
@@ -302,15 +301,6 @@ class DownloadFromWeb(AptOfflineLib.ProgressBar, GenericDownloadFunction):
                 '''width = Progress Bar width'''
                 AptOfflineLib.ProgressBar.__init__(self, width=width, total_items=total_items)
         
-class QtDownloadFromWeb(apt_offline_gui.QtProgressBar.QtProgressBar, GenericDownloadFunction):
-        '''Class for DownloadFromWeb
-        This class also inherits progressbar functionalities from
-        parent class, ProgressBar'''
-        
-        def __init__(self,progressbar,label, total_items):
-                '''width = Progress Bar width'''
-                apt_offline_gui.QtProgressBar.QtProgressBar.__init__(self,progressbar=progressbar,label=label, total_items=total_items)
-
 
 def stripper(item):
         '''Strips extra characters from "item".
@@ -498,14 +488,6 @@ def fetcher( args ):
                         AptOfflineLib.Archiver.__init__( self, lock=lock )
                         #self.lock = lock
         
-        class QtFetcherClass( QtDownloadFromWeb, AptOfflineLib.Archiver, AptOfflineLib.Checksum ):
-            def __init__( self, progress_bar, progress_label ,lock, total_items ):
-                        QtDownloadFromWeb.__init__( self, progressbar=progress_bar, label=progress_label, total_items=total_items )
-                        #ProgressBar.__init__(self, width)
-                        #self.width = width
-                        AptOfflineLib.Archiver.__init__( self, lock=lock )
-                        #self.lock = lock
-        
         if Str_DownloadDir is None:
                 tempdir = tempfile.gettempdir()
                 if os.access( tempdir, os.W_OK ) is True:
@@ -574,12 +556,7 @@ def fetcher( args ):
         # correct total count in the progress bar.
         total_items = len(FetchData['Item'])
         
-        #global FetcherInstance
-        try:
-            gui = args.qt_gui
-            FetcherInstance = QtFetcherClass(progress_bar=args.progress, progress_label=args.progress_label, lock=True, total_items=total_items )
-        except AttributeError:
-            FetcherInstance = FetcherClass( width=30, lock=True, total_items=total_items )
+        FetcherInstance = FetcherClass( width=30, lock=True, total_items=total_items )
         
         
         #INFO: Thread Support
