94bd5e3ae9e59aac9caaab137891ada2fd8e087e
Ritesh Raj Sarraf <rrs@researchut.com>
2011-02-19 23:02:07 +0530

    update genui.sh

--- apt_offline_gui/genui.sh
+++ apt_offline_gui/genui.sh
@@ -5,4 +5,5 @@ pyuic4 AptOfflineQtCreateProfile.ui > Ui_AptOfflineQtCreateProfile.py
 pyuic4  AptOfflineQtFetch.ui > Ui_AptOfflineQtFetch.py
 pyuic4 AptOfflineQtInstall.ui > Ui_AptOfflineQtInstall.py
 pyuic4 AptOfflineQtAbout.ui > Ui_AptOfflineQtAbout.py
+pyuic4  AptOfflineQtFetchOptions.ui > Ui_AptOfflineQtFetchOptions.py
 echo "Done"
