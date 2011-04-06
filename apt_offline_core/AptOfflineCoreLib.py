02a0949651946a49562a9fde3b66d9086f612c92
Ritesh Raj Sarraf <rrs@researchut.com>
2011-04-06 18:11:09 +0530

    update copyright

--- apt_offline_core/AptOfflineCoreLib.py
+++ apt_offline_core/AptOfflineCoreLib.py
@@ -80,7 +80,7 @@ figuring out if the packages are in the local cache, handling exceptions and man
 
 app_name = "apt-offline"
 version = "1.0"
-copyright = "(C) 2005 - 2010 Ritesh Raj Sarraf"
+copyright = "(C) 2005 - 2011 Ritesh Raj Sarraf"
 terminal_license = "This program comes with ABSOLUTELY NO WARRANTY.\n\
 This is free software, and you are welcome to redistribute it under\n\
 the GNU GPL Version 3 (or later) License\n"
