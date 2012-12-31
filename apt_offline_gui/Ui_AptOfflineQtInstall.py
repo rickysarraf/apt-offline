55bd57766eff1d953bacd9a9bd7b6aad0737e6da
Ritesh Raj Sarraf <rrs@debian.org>
2012-12-31 16:16:25 +0530

    Add support for downloads to directory also

--- apt_offline_gui/Ui_AptOfflineQtInstall.py
+++ apt_offline_gui/Ui_AptOfflineQtInstall.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'AptOfflineQtInstall.ui'
 #
-# Created: Mon Dec 31 15:13:47 2012
+# Created: Mon Dec 31 16:02:36 2012
 #      by: PyQt4 UI code generator 4.9.3
 #
 # WARNING! All changes made in this file will be lost!
