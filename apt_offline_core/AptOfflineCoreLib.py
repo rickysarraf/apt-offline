c5b4c18d4dc979959ea0f0ff2817a3bcb10b1697
Ritesh Raj Sarraf <rrs@debian.org>
2014-09-15 23:18:56 +0530

    Bump version to 1.5

--- apt_offline_core/AptOfflineCoreLib.py
+++ apt_offline_core/AptOfflineCoreLib.py
@@ -93,7 +93,7 @@ figuring out if the packages are in the local cache, handling exceptions and man
 
 
 app_name = "apt-offline"
-version = "1.4"
+version = "1.5"
 myCopyright = "(C) 2005 - 2014 Ritesh Raj Sarraf"
 terminal_license = "This program comes with ABSOLUTELY NO WARRANTY.\n\
 This is free software, and you are welcome to redistribute it under\n\
