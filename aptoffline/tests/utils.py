4415b66e02bfa8ddb0327562b90247926fb2ba1f
Vasudev Kamath <vasudev@copyninja.info>
2015-08-05 16:43:49 +0530

    Fix the functools.partial invocation
    Signed-off-by: Vasudev Kamath <vasudev@copyninja.info>

--- aptoffline/tests/utils.py
+++ aptoffline/tests/utils.py
@@ -3,5 +3,5 @@ from functools import partial
 
 __all__ = ['resource_path']
 
-resource_path = partial(os.path.join(os.path.dirname(__file__),
-                                     'resources'))
+resource_path = partial(os.path.join, os.path.dirname(__file__),
+                                     'resources')
