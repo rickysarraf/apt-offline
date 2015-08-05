eb077be466e1efe5ca441052a3bc30064ac25d7e
Vasudev Kamath <vasudev@copyninja.info>
2015-08-05 16:38:37 +0530

    Refactor tests for gpg signing
    * Use partial function from functool to generate resource path and place
      it in aptoffline.tests.utils module. It can be used by all tests
    * Enable logging in setUp method, additionally use resource_path to
      clean the code base a bit
    
    Signed-off-by: Vasudev Kamath <vasudev@copyninja.info>

---
+++ aptoffline/tests/utils.py
@@ -0,0 +1,7 @@
+import os
+from functools import partial
+
+__all__ = ['resource_path']
+
+resource_path = partial(os.path.join(os.path.dirname(__file__),
+                                     'resources'))
