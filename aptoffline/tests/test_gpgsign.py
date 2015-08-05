eb11b6983ff19e8e195ce0c4d9635101b45c372d
Vasudev Kamath <vasudev@copyninja.info>
2015-08-05 16:38:37 +0530

    Refactor tests for gpg signing
    * Use partial function from functool to generate resource path and place
      it in aptoffline.tests.utils module. It can be used by all tests
    * Enable logging in setUp method, additionally use resource_path to
      clean the code base a bit
    
    Signed-off-by: Vasudev Kamath <vasudev@copyninja.info>

--- aptoffline/tests/test_gpgsign.py
+++ aptoffline/tests/test_gpgsign.py
@@ -1,16 +1,15 @@
 import unittest
-import os
 from aptoffline import verifysig
+from aptoffline.tests.utils import resource_path
 
 
 class TestVerifySignature (unittest.TestCase):
+
+    def setUp(self):
+        from aptoffline import logger
+        logger.initialize_logger(verbose=True)
         
     def test_detached_sign_verify(self):
-        test_file_path = os.path.dirname(__file__)
-        v = verifysig.VerifySignature(os.path.join(
-            test_file_path,
-            'resources/signverify/keyring/debian-archive-jessie-automatic.gpg'))
-        self.assertTrue(v.detached_sign_verify(os.path.join(test_file_path,
-                                                            'resources/signverify/signature.gpg'),
-                                               os.path.join(test_file_path,
-                                                            'resources/signverify/plain.txt'))) 
+        v = verifysig.VerifySignature(resource_path('signverify/keyring/debian-archive-jessie-automatic.gpg'))
+        self.assertTrue(v.detached_sign_verify(resource_path('signverify/signature.gpg'),
+                                               resource_path('signverify/plain.txt')))
