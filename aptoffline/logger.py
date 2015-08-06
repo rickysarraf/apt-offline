e4ac38fa244652e1b0678701c177a3fc627b6aac
Vasudev Kamath <vasudev@copyninja.info>
2015-08-06 16:36:19 +0530

    Fix logger
    * Set logging level to both logger and handler
    * Set the formatting to match original apt-offline style
    
    Signed-off-by: Vasudev Kamath <vasudev@copyninja.info>

--- aptoffline/logger.py
+++ aptoffline/logger.py
@@ -1,12 +1,17 @@
 import logging
+import sys
 from logutils.colorize import ColorizingStreamHandler
 
 
 def initialize_logger(verbose):
     log = logging.getLogger('apt-offline')
     h = ColorizingStreamHandler()
+    formatter = logging.Formatter("%(levelname)s: %(message)s")
     if verbose:
+        log.setLevel(logging.DEBUG)
         h.setLevel(logging.DEBUG)
     else:
+        log.setLevel(logging.INFO)
         h.setLevel(logging.INFO)
+    h.setFormatter(formatter)
     log.addHandler(h)
