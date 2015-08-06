53ae0183cc8a2d5d3ec987775545d7904003b1cb
Vasudev Kamath <vasudev@copyninja.info>
2015-08-06 17:52:20 +0530

    Add more tests

--- aptoffline/tests/test_gpgsign.py
+++ aptoffline/tests/test_gpgsign.py
@@ -1,5 +1,5 @@
 import unittest
-from aptoffline import verifysig
+from aptoffline.verifysig import VerifySignature
 from aptoffline.tests.utils import resource_path
 
 
@@ -9,9 +9,24 @@ class TestVerifySignature (unittest.TestCase):
         from aptoffline import logger
         logger.initialize_logger(verbose=True)
 
-    def test_detached_sign_verify(self):
-        v = verifysig.VerifySignature(resource_path(
+    def test_validsign(self):
+        v = VerifySignature(resource_path(
             'signverify/keyring/debian-archive-jessie-automatic.gpg'))
         self.assertTrue(v.detached_sign_verify(resource_path(
             'signverify/signature.gpg'),
             resource_path('signverify/plain.txt')))
+
+    def test_nonexistent_keyring(self):
+        v = VerifySignature(resource_path(
+            "signverify/keyring/non-existent.gpg"))
+        self.assertFalse(v.detached_sign_verify(resource_path(
+            'signverify/signature.gpg'), resource_path(
+            'signverify/plain.txt')))
+
+    def test_nonexistent_file(self):
+        v = VerifySignature()
+        self.assertFalse(v.detached_sign_verify(resource_path(
+            'non-existent.txt'), resource_path('signverify/plain.txt')))
+        self.assertFalse(v.detached_sign_verify(resource_path(
+            'signverify/signature.gpg'), resource_path(
+                'signverify/non-existent')))
