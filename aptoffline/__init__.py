0800c5fc190016ab4b5dab99471ac4cb04560453
Vasudev Kamath <vasudev@copyninja.info>
2015-08-11 17:39:30 +0530

    Improve AptOffline base class

--- aptoffline/__init__.py
+++ aptoffline/__init__.py
@@ -1,20 +1,25 @@
+import logging
 from abc import abstractmethod
 
 
 class AptOffLine(object):
 
+    def __init__(self, type=None):
+        self.type = type
+        self.log = logging.getLogger('apt-offline')
+
     @abstractmethod
     def update(self):
-        raise NotImplementedError
+        raise NotImplemented
 
     @abstractmethod
     def upgrade(self, type="upgrade", release=None):
-        raise NotImplementedError
+        raise NotImplemented
 
     @abstractmethod
     def install_bin_packages(self, packages, release):
-        raise NotImplementedError
+        raise NotImplemented
 
     @abstractmethod
     def install_src_packages(self, packages, release, build_depends):
-        raise NotImplementedError
+        raise NotImplemented
