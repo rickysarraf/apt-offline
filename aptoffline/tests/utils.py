954bde38db040576c2a22bef9057683b4ee8606b
Vasudev Kamath <kamathvasudev@gmail.com>
2015-08-23 18:59:53 +0530

    Generate release only once and use it every where

--- aptoffline/tests/utils.py
+++ aptoffline/tests/utils.py
@@ -1,7 +1,12 @@
 import os
+import subprocess
 from functools import partial
 
-__all__ = ['resource_path']
+__all__ = ['resource_path', 'distribution_release']
 
 resource_path = partial(os.path.join, os.path.dirname(__file__),
                         'resources')
+
+distribution_release = subprocess.check_output(['lsb_release', '-r',
+                                                '-s'],
+                                               universal_newlines=True).strip()
