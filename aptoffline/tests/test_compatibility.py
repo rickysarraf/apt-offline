28a262ea1dea41ead8fd2c9d0547720d1f554202
Vasudev Kamath <kamathvasudev@gmail.com>
2015-08-23 19:00:52 +0530

    Compatibility tests with current apt-offline
    Only run if python version is <3

---
+++ aptoffline/tests/test_compatibility.py
@@ -0,0 +1,56 @@
+import re
+import sys
+import tempfile
+import unittest
+import subprocess
+from aptoffline.tests.utils import resource_path, distribution_release
+from aptoffline.logger import initialize_logger
+from aptoffline.backends.aptget import AptGet
+
+py2version = re.match('(?P<2\.d\.d)(?:.*)', sys.version)
+
+
+class TestCompatibility(unittest.TestCase):
+
+    def setUp(self):
+        self.workdir = tempfile.mkdtemp(dir=resource_path())
+        _, self.module_out = tempfile.mkstemp(dir=self.workdir())
+        _, self.aptoffline_out = tempfile.mkstemp(dir=self.workdir)
+        initialize_logger(True)
+        self.release = distribution_release
+
+    def perform_operation(self, op=None, type=None, release=None,
+                          packages=None):
+        apt = AptGet(self.module_out)
+
+        if release:
+            apt.release = release
+
+        operation = getattr(apt, op)
+
+        if op == 'upgrade':
+            operation(type=type)
+        elif op == 'update':
+            operation()
+        else:
+            operation(packages=packages)
+
+    def run_aptoffline(self, op=None, options=None):
+        cmd = ['apt-offline']
+
+        cmd.append(op)
+        if cmd in ['set', 'get']:
+            cmd.append(self.aptoffline_out)
+
+        cmd = map(cmd.append, options)
+        subprocess.check_call(cmd)
+
+    @unittest.skipUnless(py2version, ("Current apt-offline doesn't"
+                                      "work on python3"))
+    def test_update(self):
+        self.perform_operation(op='update')
+        self.run_aptoffline(op='set', options=['--update'])
+
+        with open(self.module_out) as fm:
+            with open(self.aptoffline_out) as fo:
+                self.assertEqual(fm.read(), fo.read())
