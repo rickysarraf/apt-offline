505160e94bbfb68474c0b4d77038e5053ce5ee01
Vasudev Kamath <vasudev@copyninja.info>
2015-09-04 11:57:52 +0530

    Generate version_compare variable for apt
    We need this as there was a bug in apt < 1.1~exp9 which needed sudo
    privilege for build-dep operation even when --print-uris specified.

--- aptoffline/util.py
+++ aptoffline/util.py
@@ -1,6 +1,8 @@
+import apt_pkg
 from aptsources.sourceslist import SourcesList
 
-__all__ = ['releases']
+
+__all__ = ['releases', 'apt_version_compare']
 
 
 def _releases():
@@ -8,4 +10,11 @@ def _releases():
     sentries = filter(lambda s: s.dist.strip(), sources.list)
     return {entry.dist for entry in sentries}
 
+
+def _version_apt():
+    apt_pkg.init()
+    instversion = apt_pkg.Cache()['apt'].current_ver.ver_str
+    return apt_pkg.version_compare(instversion, '1.1~exp9')
+
 releases = _releases()
+apt_version_compare = _version_apt()
