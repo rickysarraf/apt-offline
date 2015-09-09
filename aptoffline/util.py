e17a58f6aebf6fe6ff03969f12c506f3719cc537
Vasudev Kamath <vasudev@copyninja.info>
2015-09-09 13:53:50 +0530

    Use apt instead of apt_pkg for releases

--- aptoffline/util.py
+++ aptoffline/util.py
@@ -1,4 +1,5 @@
-import apt_pkg
+from apt import Cache
+from apt_pkg import version_compare
 from aptsources.sourceslist import SourcesList
 
 
@@ -12,9 +13,8 @@ def _releases():
 
 
 def _version_apt():
-    apt_pkg.init()
-    instversion = apt_pkg.Cache()['apt'].current_ver.ver_str
-    return apt_pkg.version_compare(instversion, '1.1~exp9')
+    instversion = Cache()['apt'].candidate.version
+    return version_compare(instversion, '1.1~exp9')
 
 releases = list(_releases())
 apt_version_compare = _version_apt()
