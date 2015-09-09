ea619f000fcdb5e0f4728d4410384590baeb603e
Vasudev Kamath <vasudev@copyninja.info>
2015-09-09 10:54:26 +0530

    Move pyversion detection to aptoffline.tests.utils

--- aptoffline/tests/utils.py
+++ aptoffline/tests/utils.py
@@ -1,8 +1,10 @@
 import os
+import sys
 import subprocess
 from functools import partial
+from re import match
 
-__all__ = ['resource_path', 'distribution_release']
+__all__ = ['resource_path', 'distribution_release', 'py2version']
 
 resource_path = partial(os.path.join, os.path.dirname(__file__),
                         'resources')
@@ -10,3 +12,5 @@ resource_path = partial(os.path.join, os.path.dirname(__file__),
 distribution_release = subprocess.check_output(['lsb_release', '-c',
                                                 '-s'],
                                                universal_newlines=True).strip()
+
+py2version = match('(?P<version>2\.\d\.\d)(?:.*)', sys.version)
