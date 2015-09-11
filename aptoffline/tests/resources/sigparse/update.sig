de6e26e1eb0c0246e6b3b85095a02d6fd97b9734
Vasudev Kamath <vasudev@copyninja.info>
2015-09-11 17:00:21 +0530

    Test for AptGetSigParse

---
+++ aptoffline/tests/resources/sigparse/update.sig
@@ -0,0 +1,8 @@
+'http://172.16.10.1:9999/debian/dists/sid/InRelease' 172.16.10.1:9999_debian_dists_sid_InRelease 0 
+'http://172.16.10.1:9999/debian/dists/sid/main/source/Sources.bz2' 172.16.10.1:9999_debian_dists_sid_main_source_Sources 0 
+'http://172.16.10.1:9999/debian/dists/sid/main/binary-i386/Packages.bz2' 172.16.10.1:9999_debian_dists_sid_main_binary-i386_Packages 0 
+'http://172.16.10.1:9999/debian/dists/sid/main/i18n/Translation-en.bz2' 172.16.10.1:9999_debian_dists_sid_main_i18n_Translation-en 0 
+'http://172.16.10.1:9999/debian/dists/experimental/InRelease' 172.16.10.1:9999_debian_dists_experimental_InRelease 0 
+'http://172.16.10.1:9999/debian/dists/experimental/main/source/Sources.bz2' 172.16.10.1:9999_debian_dists_experimental_main_source_Sources 0 
+'http://172.16.10.1:9999/debian/dists/experimental/main/binary-i386/Packages.bz2' 172.16.10.1:9999_debian_dists_experimental_main_binary-i386_Packages 0 
+'http://172.16.10.1:9999/debian/dists/experimental/main/i18n/Translation-en.bz2' 172.16.10.1:9999_debian_dists_experimental_main_i18n_Translation-en 0 
---
+++ aptoffline/tests/test_aptgetsigparse.py
@@ -0,0 +1,34 @@
+"""Tests for confirming AptGetSigParse functionality.
+"""
+import os
+
+from .base import AptOfflineTests
+from testtools.content import text_content
+from testtools.matchers import GreaterThan
+from aptoffline.backends.aptget import AptGetSigParse
+
+
+class TestAptGetSigParse(AptOfflineTests):
+
+    def setUp(self):
+        super(TestAptGetSigParse, self).setUp()
+        self.sigparse_dir = self.resource_path('sigparse')
+
+    def test_update_sigparse(self):
+        parser = AptGetSigParse(os.path.join(self.sigparse_dir,
+                                             'update.sig'))
+        for item in parser:
+            self.addDetail('sigitem', text_content(repr(item)))
+            self.assertEqual(item.size, '0')
+            self.assertFalse(hasattr(item, 'md5sum'))
+        
+    def test_nonupdate_sigparse(self):
+        parser = AptGetSigParse(os.path.join(self.sigparse_dir,
+                                             'apt-offline.sig'))
+        for item in parser:
+            self.addDetail('sigitem', text_content(repr(item)))
+            self.assertThat(int(item.size), GreaterThan(0))
+            self.assertTrue(hasattr(item, 'md5sum'))
+        
+    def tearDown(self):
+        super(TestAptGetSigParse, self).tearDown()
