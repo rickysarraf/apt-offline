1e112376d8547a71174a7c8ee51f6217fe452e37
Vasudev Kamath <vasudev@copyninja.info>
2015-10-06 15:54:34 +0530

    Capture checksum_type also from the item line

--- aptoffline/tests/test_aptgetsigparse.py
+++ aptoffline/tests/test_aptgetsigparse.py
@@ -16,19 +16,20 @@ class TestAptGetSigParse(AptOfflineTests):
         self.sigparse_dir = partial(self.resource_path, 'sigparse')
 
     def test_update_sigparse(self):
-        parser = AptGetSigParse(os.path.join(self.sigparse_dir,
-                                             'update.sig'))
+        parser = AptGetSigParse(os.path.join(self.sigparse_dir(
+            'update.sig')))
         for item in parser:
             self.addDetail('sigitem', text_content(repr(item)))
             self.assertEqual(item.size, '0')
-            self.assertFalse(hasattr(item, 'md5sum'))
+            self.assertFalse(hasattr(item, 'checksum'))
 
     def test_nonupdate_sigparse(self):
         parser = AptGetSigParse(self.sigparse_dir('apt-offline.sig'))
         for item in parser:
             self.addDetail('sigitem', text_content(repr(item)))
             self.assertThat(int(item.size), GreaterThan(0))
-            self.assertTrue(hasattr(item, 'md5sum'))
+            self.assertTrue(hasattr(item, 'checksum'))
+            self.assertTrue(hasattr(item, 'checksum_type'))
 
     def test_parsed_updatevalues(self):
         parser = AptGetSigParse(self.sigparse_dir('update-1line.sig'))
@@ -40,7 +41,7 @@ class TestAptGetSigParse(AptOfflineTests):
                          '172.16.10.1:9999_debian_dists_sid'
                          '_main_binary-i386_Packages')
         self.assertEqual(int(parser[0].size), 0)
-        self.assertFalse(hasattr(parser[0], 'md5sum'))
+        self.assertFalse(hasattr(parser[0], 'checksum'))
 
     def test_parsed_nonupdatevalues(self):
         parser = AptGetSigParse(
@@ -54,7 +55,8 @@ class TestAptGetSigParse(AptOfflineTests):
                          'libgraphite2-3_1.2.4-3_i386.deb')
         self.assertEqual(int(parser[0].size),
                          57966)
-        self.assertEqual(parser[0].md5sum,
+        self.assertEqual(parser[0].checksum_type, 'MD5Sum')
+        self.assertEqual(parser[0].checksum,
                          '9b85dcfb537b09b7ccac4a68e164429c')
 
     def tearDown(self):
