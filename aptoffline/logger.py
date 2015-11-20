1d4a2bca973e2a4df6c3139d8c7595281a8fab6d
Vasudev Kamath <kamathvasudev@gmail.com>
2015-11-20 21:39:49 +0530

    Take over line overwriting variable from old code
    Take the screen clean up variables from old apt-offline. Not sure how
    this works but still.

--- aptoffline/logger.py
+++ aptoffline/logger.py
@@ -1,6 +1,14 @@
 from logging import getLogger, Formatter, DEBUG, INFO
 from .packages.logutils.colorize import ColorizingStreamHandler
 
+#These are spaces which will overwrite the progressbar left mess
+LINE_OVERWRITE_SMALL = " " * 10
+LINE_OVERWRITE_MID = " " * 30
+LINE_OVERWRITE_FULL = " " * 60
+
+__all__ = ['LINE_OVERWRITE_FULL', 'LINE_OVERWRITE_MID',
+           'LINE_OVERWRITE_SMALL', 'initialize_logger']
+
 
 def initialize_logger(verbose):
     log = getLogger('apt-offline')
