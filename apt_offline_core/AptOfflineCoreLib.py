96387b9153d36c48c556f11f7ad75c481c29d961
Ritesh Raj Sarraf <rrs@debian.org>
2016-02-21 23:32:00 +0530

    Change msg type to emphasize the message

--- apt_offline_core/AptOfflineCoreLib.py
+++ apt_offline_core/AptOfflineCoreLib.py
@@ -1360,7 +1360,7 @@ def fetcher( args ):
         if len(errlist) > 0:
                 log.msg("\n\n")
                 log.err("The following files failed to be downloaded.\n")
-                log.msg("Not all errors are fatal. For eg. Translation files are not present on all mirrors.\n")
+                log.success("Not all errors are fatal. For eg. Translation files are not present on all mirrors.\n")
                 for error in errlist:
                         log.err("%s failed.\n" % (error))
         if args.bundle_file:
