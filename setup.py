a5eb2b7f5d6095a88bdf803cb82e964bfeee0d35
Ritesh Raj Sarraf <rrs@debian.org>
2017-07-01 21:13:21 +0545

    Bump version number to 1.8.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import distutils.core
 
 distutils.core.setup(
     name='apt-offline',
-    version='1.8.0',
+    version='1.8.1',
     author='Ritesh Raj Sarraf',
     author_email='rrs@researchut.com',
     url='http://apt-offline.alioth.debian.org',
