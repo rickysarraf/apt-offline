a5262d0d691fe506ff8aed8d7b680bc71ea643e5
Matthias Blümel <matthias.bluemel@krumedia.com>
2017-07-03 16:32:31 +0200

    support https client-authentication

--- apt_offline_core/AptOfflineCoreLib.py
+++ apt_offline_core/AptOfflineCoreLib.py
@@ -25,6 +25,7 @@ import time
 import shutil
 import platform
 import string
+import ssl
 import urllib2
 import httplib
 import Queue
@@ -777,6 +778,8 @@ def fetcher( args ):
         Str_BundleFile = args.bundle_file
         Str_ProxyHost = args.proxy_host
         Str_ProxyPort = args.proxy_port
+        Str_HttpsCertFile = args.https_cert_file
+        Str_HttpsKeyFile = args.https_key_file
         Bool_BugReports = args.deb_bugs
         global guiTerminateSignal
         
@@ -801,6 +804,14 @@ def fetcher( args ):
                         opener = urllib2.build_opener(proxy_support)
                         urllib2.install_opener(opener)
                         log.verbose("Proxy successfully set up with Host %s and default port\n" % (Str_ProxyHost) )
+        
+        if Str_HttpsCertFile and Str_HttpsKeyFile:
+                log.verbose("cert-file:" + Str_HttpsCertFile + " key-file:" + Str_HttpsKeyFile)
+                context = ssl.create_default_context()
+                context.load_cert_chain(Str_HttpsCertFile, Str_HttpsKeyFile)
+                opener = urllib2.build_opener(urllib2.HTTPSHandler(context=context))
+                urllib2.install_opener(opener)
+                log.verbose("SSL Client Authentication successfully set up with certificate file %s and key file %s\n" % (Str_HttpsCertFile, Str_HttpsKeyFile))
         
         #INFO: Python 2.5 has hashlib which supports sha256
         # If we don't have Python 2.5, disable MD5/SHA256 checksum
@@ -2008,6 +2019,12 @@ def main():
         
         parser_get.add_argument("--proxy-port", dest="proxy_port",
 						help="Proxy port number to use", type=int, default=None)
+        
+        parser_get.add_argument("--https-cert-file", dest="https_cert_file",
+						help="Certificate file for https client authentication", type=str, default=None)
+        
+        parser_get.add_argument("--https-key-file", dest="https_key_file",
+						help="Certificate key for https client authentication", type=str, default=None)
         
         # INSTALL command options
         parser_install = subparsers.add_parser('install', parents=[global_options])
