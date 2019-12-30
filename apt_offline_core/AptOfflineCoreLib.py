58291032ce1a7e1743b1e4663ba0558394b33b43
Ritesh Raj Sarraf <rrs@debian.org>
2019-12-30 15:46:14 +0530

    Update copyright year

--- apt_offline_core/AptOfflineCoreLib.py
+++ apt_offline_core/AptOfflineCoreLib.py
@@ -1,6 +1,6 @@
 
 ############################################################################
-#    Copyright (C) 2005, 2015 Ritesh Raj Sarraf                            #
+#    Copyright (C) 2005, 2020 Ritesh Raj Sarraf                            #
 #    rrs@researchut.com                                                    #
 #                                                                          #
 #    This program is free software; you can redistribute it and/or modify  #
@@ -102,7 +102,7 @@ figuring out if the packages are in the local cache, handling exceptions and man
 
 app_name = "apt-offline"
 version = "1.8.1"
-myCopyright = "(C) 2005 - 2017 Ritesh Raj Sarraf"
+myCopyright = "(C) 2005 - 2020 Ritesh Raj Sarraf"
 terminal_license = "This program comes with ABSOLUTELY NO WARRANTY.\n\
 This is free software, and you are welcome to redistribute it under\n\
 the GNU GPL Version 3 (or later) License\n"
