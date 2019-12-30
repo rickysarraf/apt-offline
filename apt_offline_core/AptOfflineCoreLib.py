ae5c4d3de3350e17a4607f4401bc04327e48f1e1
Ritesh Raj Sarraf <rrs@debian.org>
2019-12-30 15:47:39 +0530

    Update version to 1.8.2

--- apt_offline_core/AptOfflineCoreLib.py
+++ apt_offline_core/AptOfflineCoreLib.py
@@ -101,7 +101,7 @@ figuring out if the packages are in the local cache, handling exceptions and man
 
 
 app_name = "apt-offline"
-version = "1.8.1"
+version = "1.8.2"
 myCopyright = "(C) 2005 - 2020 Ritesh Raj Sarraf"
 terminal_license = "This program comes with ABSOLUTELY NO WARRANTY.\n\
 This is free software, and you are welcome to redistribute it under\n\
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import distutils.core
 
 distutils.core.setup(
     name='apt-offline',
-    version='1.8.1',
+    version='1.8.2',
     author='Ritesh Raj Sarraf',
     author_email='rrs@researchut.com',
     url='http://apt-offline.alioth.debian.org',
