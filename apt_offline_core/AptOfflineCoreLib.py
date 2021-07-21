01f6c66a6aeeced1b6849c1874ddd53f92e55a7f
Ritesh Raj Sarraf <rrs@debian.org>
2021-07-21 22:42:36 +0530

    The check for cdrom:// type sources shouldn't be needed any more. See previous commit

--- apt_offline_core/AptOfflineCoreLib.py
+++ apt_offline_core/AptOfflineCoreLib.py
@@ -1279,13 +1279,6 @@ def fetcher( args ):
 
                 (key, item) = request
 
-                # On many boxes, the cdrom apt repository will be enabled.
-                # For now, let's skip the cdrom repository items.
-                if item.startswith("\'cdrom"):
-                    log.err("cdrom apt repository not supported. Skipping %s\n" % (item))
-                    return True
-
-
                 #INFO: Everything
                 (url, pkgFile, download_size, checksum) = stripper(item)
                 thread_name = threading.currentThread().getName()
