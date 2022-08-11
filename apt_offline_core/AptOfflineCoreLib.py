39d0eb6297b58eda245008c2d07a46c979b57ac1
Ritesh Raj Sarraf <rrs@debian.org>
2022-08-11 20:48:18 +0530

    Bump apt-offline version to 1.8.5

--- apt_offline_core/AptOfflineCoreLib.py
+++ apt_offline_core/AptOfflineCoreLib.py
@@ -100,7 +100,7 @@ figuring out if the packages are in the local cache, handling exceptions and man
 
 
 app_name = "apt-offline"
-version = "1.8.4"
+version = "1.8.5"
 myCopyright = "(C) 2005 - 2022 Ritesh Raj Sarraf"
 terminal_license = "This program comes with ABSOLUTELY NO WARRANTY.\n\
 This is free software, and you are welcome to redistribute it under\n\
